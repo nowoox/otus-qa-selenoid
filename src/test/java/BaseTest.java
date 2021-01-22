c191bbf99188c7f32449acff3251f2d17146bbff
nowoox <nowoox@gmail.com>
2021-01-22 23:56:24 +0300

    Initial commit

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,57 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.PageFactory;
+import pages.MainPage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+    private MainPage mainPage;
+    private String port;
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        port = setEnvironment("src/main/resources/SetRemoteEnv.bat");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        capabilities.setVersion("80.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        driver = new RemoteWebDriver(
+                new URL("http://0.0.0.0:" + port + "/wd/hub"),
+                capabilities
+        );
+        mainPage = PageFactory.initElements(driver, MainPage.class);
+    }
+
+    private String setEnvironment(String path) throws IOException {
+        InputStream inputStream = Runtime.getRuntime().exec(new String[]{path}).getInputStream();
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        StringBuffer stringBuffer = new StringBuffer();
+        String string;
+        while ((string = bufferedReader.readLine()) != null){
+            stringBuffer.append(string);
+        }
+        String resultString = stringBuffer.toString();
+        port = resultString.substring(resultString.indexOf("SelenoidPort selenoid is :") + 27,
+                resultString.indexOf("/tcp"));
+        return port;
+    }
+
+    @AfterEach
+    public void setDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,13 @@
+import pages.MainPage;
+import org.junit.jupiter.api.Test;
+
+public class SampleTest extends BaseTest{
+
+    private MainPage mainPage;
+
+    @Test
+    public void Test() {
+        System.out.println("Hello");
+    }
+
+}
