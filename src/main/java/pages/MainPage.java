3feaa3467afba8e9d3050c60f203f1dbf75f4208
nowoox <nowoox@gmail.com>
2021-02-14 00:26:54 +0300

    Refactoring + Assert added

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,8 +1,10 @@
 package pages;
 
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import sun.awt.geom.AreaOp;
 
 public class MainPage {
 
@@ -19,5 +21,10 @@ public class MainPage {
 
     public void typeQuery(String query) {
         driver.findElement(By.name("q")).sendKeys(query);
+    }
+
+    public void checkQuery(String query) {
+        Assertions.assertTrue(driver.findElement(By.xpath("//span[contains(text(), '" + query + "')]")).isDisplayed());
+        System.out.println(query + " found");
     }
 }
--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,17 +1,21 @@
 import Base.BaseTest;
-import pages.MainPage;
 import org.junit.jupiter.api.Test;
+import pages.MainPage;
 
 public class SampleTest extends BaseTest {
 
     @Test
     public void Test() {
 
+        String q = "Wikipedia";
+
         mainPage = new MainPage(driver);
 
-        mainPage.typeQuery("Wikipedia");
+        mainPage.typeQuery(q);
 
         mainPage.clickQuery();
+
+        mainPage.checkQuery(q);
 
     }
 
